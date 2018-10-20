f3e4fcb570c5d1c1917eb0c7d84675552113aa7d
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-20 12:57:26 -0400

    Add routing and about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import Home from './components/home'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from './components/home';
+import About from './components/about';
 import logo from './logo.svg';
 import './App.css';
 
+// for routes, home needs to come last
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
+    <div>
+      <BrowserRouter>
+        <div>
+          <div className="App">
+          <Switch>
+          <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
+            <Route path={`${process.env.PUBLIC_URL}/`} component={Home} />
+          </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
       </div>
     );
   }
